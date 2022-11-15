b2aca729369795f84a48ca0ae59521d6a1266d2c
ssvegaraju <ssvegaraju@yahoo.co.in>
2022-11-15 10:19:58 -0800

    test: add csv parsed tests to cases

---
+++ src/comboTest/testStubs.ts
@@ -0,0 +1,67 @@
+import { ScopeRule, SMARTConfig } from '../smartConfig';
+
+export const scopeRule = (): ScopeRule => ({
+    patient: {
+        read: ['read', 'vread', 'search-type', 'search-system', 'history-instance', 'history-type', 'history-system'],
+        write: ['create', 'transaction'],
+    },
+    user: {
+        read: ['read', 'vread', 'search-type', 'search-system', 'history-instance', 'history-type', 'history-system'],
+        write: ['update', 'patch', 'create', 'delete', 'transaction'],
+    },
+    system: {
+        // "read" allows system export and group export
+        read: ['read'],
+        write: [],
+    },
+});
+export const expectedAud = 'api://default';
+export const expectedIss = 'https://dev-6460611.okta.com/oauth2/default';
+export const baseAuthZConfig = (): SMARTConfig => ({
+    version: 1.0,
+    scopeKey: 'scp',
+    scopeRule: scopeRule(),
+    expectedAudValue: expectedAud,
+    expectedIssValue: expectedIss,
+    fhirUserClaimPath: 'fhirUser',
+    launchContextPathPrefix: 'ext.launch_response_',
+    jwksEndpoint: `${expectedIss}/jwks`,
+});
+export const apiUrl = 'https://fhir.server.com/dev';
+export const id = 'id';
+export const patientId = `Patient/${id}`;
+export const practitionerId = `Practitioner/${id}`;
+export const patientIdentity = `${apiUrl}/${patientId}`;
+export const practitionerIdentity = `${apiUrl}/${practitionerId}`;
+export const sub = 'test@test.com';
+
+export const patientContext: any = {
+    ext: { launch_response_patient: patientIdentity },
+};
+export const patientFhirUser: any = {
+    fhirUser: patientIdentity,
+};
+export const practitionerFhirUser: any = {
+    fhirUser: practitionerIdentity,
+};
+
+export const baseAccessNoScopes: any = {
+    ver: 1,
+    jti: 'AT.6a7kncTCpu1X9eo2QhH1z_WLUK4TyV43n_9I6kZNwPY',
+    iss: expectedIss,
+    aud: expectedAud,
+    iat: Math.floor(Date.now() / 1000) - 1,
+    exp: Math.floor(Date.now() / 1000) + 60 * 60,
+    cid: '0oa8muazKSyk9gP5y5d5',
+    uid: '00u85ozwjjWRd17PB5d5',
+    sub,
+};
+
+export const getFhirUserType = (fhirUser: string): string | undefined => {
+    if (fhirUser === 'practitionerFhirUser') {
+        return practitionerIdentity;
+    } else if (fhirUser === 'patientFhirUser') {
+        return patientIdentity;
+    }
+    return undefined;
+}
--- src/comboTest/verifyAccessToken-combo.test.ts
+++ src/comboTest/verifyAccessToken-combo.test.ts
@@ -3,35 +3,19 @@
 // import { filterOutUnusableScope } from './smartScopeHelper';
 import * as path from 'path';
 import { VerifyAccessTokenRequest } from 'fhir-works-on-aws-interface';
-import { ScopeRule, SMARTConfig } from '../smartConfig';
 import { SMARTHandler } from '../smartHandler';
 import * as smartAuthorizationHelper from '../smartAuthorizationHelper';
+import * as testStubs from './testStubs';
 
 // const sync = require('csv-parse/lib/sync');
 
 const { load } = require('csv-load-sync');
 
-const scopeRule = (): ScopeRule => ({
-    patient: {
-        read: ['read', 'vread', 'search-type', 'search-system', 'history-instance', 'history-type', 'history-system'],
-        write: ['create', 'transaction'],
-    },
-    user: {
-        read: ['read', 'vread', 'search-type', 'search-system', 'history-instance', 'history-type', 'history-system'],
-        write: ['update', 'patch', 'create', 'delete', 'transaction'],
-    },
-    system: {
-        // "read" allows system export and group export
-        read: ['read'],
-        write: [],
-    },
-});
-
 interface CsvRow {
-    patientContext: string;
-    fhirUser: string;
+    patientContext: object | undefined;
+    fhirUser: string | undefined;
     operation: string;
-    resourceType: string;
+    resourceType: string | undefined;
     id: string;
     vid: string;
     fhirServiceBaseUrl: string;
@@ -58,117 +42,100 @@ interface CsvRow {
 const getScopesFromResult = (result: any) => {
     const scopes: string[] = [];
     Object.entries(result).forEach(([key, value]) => {
-        if (key.startsWith('system/') || key.startsWith('user/') || (key.startsWith('/patient/') && value === key)) {
+        if (key.startsWith('system/') || key.startsWith('user/') || (key.startsWith('patient/') && value === key)) {
             scopes.push(key);
         }
     });
     return scopes;
 };
+
 // TODO : use path to get the right path
 const csv: CsvRow[] = load(path.resolve(__dirname, './params/VerifyAccessToken-NoBulkDataAuth-params.csv'), {
     convert: {
         isUserScopeAllowedForSystemExport: (s: string) => s === 'true',
+        resourceType: (s: string) => (s === 'N/A') ? undefined : s,
+        fhirUser: (s: string) => testStubs.getFhirUserType(s),
+        patientContext: (s: string) => (s === 'N/A') ? undefined : testStubs.patientContext,
     },
 });
-const clonedScopeRule = scopeRule();
+const clonedScopeRule = testStubs.scopeRule();
 const testCasesFromCSV: any[] = [];
-csv.forEach((row) => {
-    const testInput: any = {
-        scopes: getScopesFromResult(row),
-        scopeRule: clonedScopeRule,
-        // reqOperation: row.operation as TypeOperation | SystemOperation,
-        // isUserScopeAllowedForSystemExport: row.isUserScopeAllowedForSystemExport,
-        // reqResourceType: row.reqResourceType,
+// [
+//     'Read and Write Access: initiate-export',
+//     {
+//         accessToken: 'fake',
+//         operation: 'read',
+//         resourceType: '',
+//         bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
+//     },
+//     { ...baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...practitionerFhirUser },
+// ]
+const testCases: any[] = [];
+csv.forEach((row, index) => {
+    const result: any = {};
+
+    result.testName = `Combo Test Row ${index}`;
+    result.request = {
+        accessToken: 'fake',
+        operation: row.operation,
+        resourceType: row.resourceType || '',
         bulkDataAuth: undefined,
-        patientContext: row.patientContext,
+    };
+    result.decodedAccessToken = {
+        ...testStubs.baseAccessNoScopes,
+        scp: getScopesFromResult(row),
         fhirUser: row.fhirUser,
     };
-    testCasesFromCSV.push(testInput);
+    if (row.patientContext) {
+        result.decodedAccessToken = {
+            ...result.decodedAccessToken,
+            ...row.patientContext,
+        }
+    }
+    testCases.push([JSON.stringify(result, null, 2), result]);
 });
 
-// TODO: Combine common set up to a separate util file
-const expectedAud = 'api://default';
-const expectedIss = 'https://dev-6460611.okta.com/oauth2/default';
-const baseAuthZConfig = (): SMARTConfig => ({
-    version: 1.0,
-    scopeKey: 'scp',
-    scopeRule: scopeRule(),
-    expectedAudValue: expectedAud,
-    expectedIssValue: expectedIss,
-    fhirUserClaimPath: 'fhirUser',
-    launchContextPathPrefix: 'ext.launch_response_',
-    jwksEndpoint: `${expectedIss}/jwks`,
-});
-const apiUrl = 'https://fhir.server.com/dev';
-const id = 'id';
-const patientId = `Patient/${id}`;
-const practitionerId = `Practitioner/${id}`;
-const patientIdentity = `${apiUrl}/${patientId}`;
-const practitionerIdentity = `${apiUrl}/${practitionerId}`;
-const sub = 'test@test.com';
-
-const patientContext: any = {
-    ext: { launch_response_patient: patientIdentity },
-};
-const patientFhirUser: any = {
-    fhirUser: patientIdentity,
-};
-const practitionerFhirUser: any = {
-    fhirUser: practitionerIdentity,
-};
-
-const baseAccessNoScopes: any = {
-    ver: 1,
-    jti: 'AT.6a7kncTCpu1X9eo2QhH1z_WLUK4TyV43n_9I6kZNwPY',
-    iss: expectedIss,
-    aud: expectedAud,
-    iat: Math.floor(Date.now() / 1000) - 1,
-    exp: Math.floor(Date.now() / 1000) + 60 * 60,
-    cid: '0oa8muazKSyk9gP5y5d5',
-    uid: '00u85ozwjjWRd17PB5d5',
-    sub,
-};
 // End TODO
 
 describe('verifyAccessToken-combo', () => {
     // Example array of test cases
     // TODO: next step is to convert csv file loaded to follow the same format
-    const cases: (string | boolean | VerifyAccessTokenRequest | any)[][] = [
-        [
-            'Read and Write Access: initiate-export',
-            {
-                accessToken: 'fake',
-                operation: 'read',
-                resourceType: '',
-                bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
-            },
-            { ...baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...practitionerFhirUser },
-        ],
-        [
-            'Read and Write Access: initiate-export',
-            {
-                accessToken: 'fake',
-                operation: 'read',
-                resourceType: '',
-                bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
-            },
-            { ...baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...patientFhirUser },
-        ],
-        [
-            'Read and Write Access: initiate-export',
-            {
-                accessToken: 'fake',
-                operation: 'read',
-                resourceType: '',
-                bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
-            },
-            { ...baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...patientContext },
-        ],
-    ];
+    // const cases: (string | boolean | VerifyAccessTokenRequest | any)[][] = [
+    //     [
+    //         'Read and Write Access: initiate-export',
+    //         {
+    //             accessToken: 'fake',
+    //             operation: 'read',
+    //             resourceType: '',
+    //             bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
+    //         },
+    //         { ...testStubs.baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...testStubs.practitionerFhirUser },
+    //     ],
+    //     [
+    //         'Read and Write Access: initiate-export',
+    //         {
+    //             accessToken: 'fake',
+    //             operation: 'read',
+    //             resourceType: '',
+    //             bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
+    //         },
+    //         { ...testStubs.baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...testStubs.patientFhirUser },
+    //     ],
+    //     [
+    //         'Read and Write Access: initiate-export',
+    //         {
+    //             accessToken: 'fake',
+    //             operation: 'read',
+    //             resourceType: '',
+    //             bulkDataAuth: { exportType: 'system', operation: 'initiate-export' },
+    //         },
+    //         { ...testStubs.baseAccessNoScopes, scp: ['user/*.*', 'patient/*.write'], ...testStubs.patientContext },
+    //     ],
+    // ];
 
-    const authZConfig = baseAuthZConfig();
-    const authZHandler: SMARTHandler = new SMARTHandler(authZConfig, apiUrl, '4.0.1', undefined, undefined, true);
-    test.each(cases)('CASE: %p', async (_firstArg, request, decodedAccessToken) => {
+    const authZConfig = testStubs.baseAuthZConfig();
+    const authZHandler: SMARTHandler = new SMARTHandler(authZConfig, testStubs.apiUrl, '4.0.1', undefined, undefined, true);
+    test.each(testCases)('CASE: %s', async (testCaseString, testCase) => {
         const authZHandlerWithAnotherApiURL: SMARTHandler = new SMARTHandler(
             authZConfig,
             'https://some-server.com',
@@ -178,16 +145,16 @@ describe('verifyAccessToken-combo', () => {
             true,
         );
 
-        const requestWithFhirServiceBaseUrl = { ...request, fhirServiceBaseUrl: apiUrl };
+        const requestWithFhirServiceBaseUrl = { ...testCase.request, fhirServiceBaseUrl: testStubs.apiUrl };
 
         // Handling mocking modules when code is in TS: https://stackoverflow.com/a/60693903/14310364
         jest.spyOn(smartAuthorizationHelper, 'verifyJwtToken').mockImplementation(() =>
-            Promise.resolve(decodedAccessToken),
+            Promise.resolve(testCase.decodedAccessToken),
         );
 
         // TODO: Snapshot contains timestamp, need to update logic to static or it fails on rerun
         try {
-            await expect(authZHandler.verifyAccessToken(<VerifyAccessTokenRequest>request)).resolves.toMatchSnapshot();
+            await expect(authZHandler.verifyAccessToken(<VerifyAccessTokenRequest>testCase.request)).resolves.toMatchSnapshot();
         } catch (e) {
             expect(e).toMatchSnapshot();
         }
