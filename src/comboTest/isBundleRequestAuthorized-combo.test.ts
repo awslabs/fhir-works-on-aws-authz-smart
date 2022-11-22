f242854ddb47c54b20e7e6ada7c48a18fb8ae667
ssvegaraju <ssvegaraju@yahoo.co.in>
2022-11-22 11:25:31 -0800

    test: isBundleRequestAuthorized combo tests

---
+++ src/comboTest/isBundleRequestAuthorized-combo.test.ts
@@ -0,0 +1,113 @@
+import { AuthorizationBundleRequest } from 'fhir-works-on-aws-interface';
+import { SMARTHandler } from '../smartHandler';
+import * as smartAuthorizationHelper from '../smartAuthorizationHelper';
+import * as testStubs from './testStubs';
+import TestCaseUtil, { BaseCsvRow } from './testCaseUtil.test';
+import { convertNAtoUndefined } from './testStubs';
+
+interface CsvRow extends BaseCsvRow {
+    resourceBody: string | undefined;
+    fhirServiceBaseUrl: string;
+    'patient/Patient.read': string;
+    'patient/*.read': string;
+    'user/Patient.read': string;
+    'user/*.read': string;
+    'system/Patient.read': string;
+    'system/*.read': string;
+    isUserScopeAllowedForSystemExport: boolean;
+}
+const testCaseUtil = new TestCaseUtil<CsvRow>(
+    './params/isBundleRequestAuthorized-params.csv',
+    'isBundleRequestAuthorized',
+);
+
+const loadAndPrepareTestCases = () => {
+    const testCases: any[] = [];
+    const csv = testCaseUtil.loadTestCase({
+        fhirServiceBaseUrl: (s: string) => convertNAtoUndefined(s),
+        resourceBody: (s: string) => convertNAtoUndefined(s),
+    });
+
+    csv.forEach((inputRow, index) => {
+        const testCase: any = {};
+        const row = inputRow.csvRow;
+        testCase.testName = `Combo Test Row ${index}`;
+        testCase.request = {
+            accessToken: 'fake',
+            operation: row.operation,
+            resourceType: row.resourceBody || '',
+            fhirServiceBaseUrl: testStubs.convertToBaseUrl(row.fhirServiceBaseUrl),
+        };
+        testCase.decodedAccessToken = {
+            ...testStubs.baseAccessNoScopes,
+            scp: testCaseUtil.getScopesFromResult(row),
+            fhirUser: testStubs.getFhirUserType(row.fhirUser),
+        };
+        testCase.isUserScopeAllowedForSystemExport = row.isUserScopeAllowedForSystemExport;
+        if (row.patientContext) {
+            testCase.decodedAccessToken = {
+                ...testCase.decodedAccessToken,
+                ...testStubs.patientContext,
+            };
+        }
+        testCases.push([JSON.stringify(testCase, null, 2), testCase]);
+    });
+    return testCases;
+};
+
+describe('isBundleRequestAuthorized-BulkDataAuth-combo', () => {
+    const testResults: any[] = [];
+    // TODO: Update for bulk
+    const keysToOutput: any[] = [
+        { field: 'testName', title: 'Test Number' },
+        { field: 'request.operation', title: 'Operation' },
+        { field: 'request.resourceBody', title: ' Resource Body' },
+        { field: 'decodedAccessToken.fhirUser', title: 'fhirUser' },
+        { field: 'decodedAccessToken.ext.launch_response_patient', title: 'Patient in Context' },
+        { field: 'message', title: 'Error' },
+        { field: 'usableScopes', title: 'Usable Scopes' },
+        { field: 'decodedAccessToken.scp', title: 'Scopes' },
+    ];
+    afterAll(async () => {
+        await testCaseUtil.writeTestResultsToCsv(testResults, keysToOutput);
+    });
+    const testCases = loadAndPrepareTestCases();
+    const authZConfig = testStubs.baseAuthZConfig();
+    const authZHandlerUserScope: SMARTHandler = new SMARTHandler(
+        authZConfig,
+        testStubs.apiUrl,
+        '4.0.1',
+        undefined,
+        undefined,
+        true,
+    );
+    const authZHandlerNoUserScope: SMARTHandler = new SMARTHandler(
+        authZConfig,
+        testStubs.apiUrl,
+        '4.0.1',
+        undefined,
+        undefined,
+        false,
+    );
+    test.each(testCases)('CASE: %s', async (testCaseString, testCase) => {
+        // Handling mocking modules when code is in TS: https://stackoverflow.com/a/60693903/14310364
+        jest.spyOn(smartAuthorizationHelper, 'verifyJwtToken').mockImplementation(() =>
+            Promise.resolve(testCase.decodedAccessToken),
+        );
+        let testResult: any;
+        const authZHandler = testCase.isUserScopeAllowedForSystemExport
+            ? authZHandlerUserScope
+            : authZHandlerNoUserScope;
+        // TODO: Snapshot contains timestamp, need to update logic to static or it fails on rerun
+        try {
+            testResult = await authZHandler.isBundleRequestAuthorized(<AuthorizationBundleRequest>testCase.request);
+
+            expect(testResult).toMatchSnapshot();
+        } catch (e) {
+            // TODO: append errors to output file
+            testResult = { message: (e as Error).message };
+            expect(e).toMatchSnapshot();
+        }
+        testResults.push({ ...testCase, ...testResult });
+    });
+});
